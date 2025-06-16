4264a38b214b32bfc00d3cb0dc995fb4fb131645
Andréa <andrea.marnier@gmail.com>
2025-06-16 13:23:20 +0200

    story 1 - quest 3

---
+++ stories/01_Echoes_of_Enigmatus/quest_03.py
@@ -0,0 +1,61 @@
+import argparse
+import re
+
+from pathlib import Path
+from time import time
+
+def _parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--part", "-p",
+        type=int,
+        choices={1, 2, 3},
+        help="Set puzzle part"
+    )
+    args = parser.parse_args()
+    if not args.part:
+        parser.error("Which part are you solving?")
+    return args
+
+def get_clock(snail: tuple) -> int:
+    x, y = snail
+    return x + y - 1
+
+def pass_time(snail: tuple, days: int) -> tuple:
+    clock = get_clock(snail)
+    x, _ = snail
+    future_x = (((x - 1) + days ) % clock) + 1
+    return (future_x, clock + 1 - future_x)
+
+def position(snail: tuple) -> int:
+    x, y = snail
+    return x + 100 * y
+
+def chinese_remainder_theorem() -> int:
+    n = 1
+    for snail in data:
+        n *= get_clock(snail)
+    result = 0
+    for snail in data:
+        _, y = snail
+        clock = get_clock(snail)
+        n_except = int(n / clock)
+        mul = 1
+        while (mul * n_except) % clock != 1:
+            mul += 1
+        result += (y - 1) * mul * n_except
+    return result % n
+
+if __name__ == "__main__":
+    args = _parse_args()
+    t = time()
+    with Path(f"inputs/{Path(__file__).stem}_{args.part}.txt").open("r") as file:
+        data = [
+            tuple(int(x) for x in re.findall(r"(\d+)", raw))
+            for raw in file.read().strip().split("\n")
+        ]
+    if args.part == 1:
+        print(sum(position(pass_time(snail, 100)) for snail in data))
+    else:
+        print(chinese_remainder_theorem())
+    print(time() - t)
--- stories/01_Echoes_of_Enigmatus/stats.md
+++ stories/01_Echoes_of_Enigmatus/stats.md
@@ -1,3 +1,4 @@
+I was late to the party and didn't expect any ranking but hey! Here I am, 48th by point!
 
 |Quest|Part|Global Rank|Global Time|Local Time|
 |---|---|---|---|---|
@@ -7,3 +8,12 @@
 |2|I|60|8d 20h 54m 53s 535ms|9h 58m 04s 919ms|
 |2|II|57|8d 22h 11m 00s 618ms|11h 14m 12s 002ms|
 |2|III|53|9d 06h 40m 07s 861ms|19h 43m 19s 245ms|
+|3|I|52|9d 07h 15m 16s 812ms|13m 35s 459ms|
+|3|II|52|9d 07h 22m 45s 632ms|21m 04s 279ms|
+|3|III|49|9d 13h 15m 49s 897ms|6h 14m 08s 544ms|
+
+
+Bruteforce was working on Q3 P2, and as I was thinking "I should go back to this afterward, it's not nice", well... P3 had me do it ^^.
+Super happy to remember about the chinese remainder theorem!
+
+Nice little story. I enjoyed solving some puzzles, but not for too long. This is a nice format.
