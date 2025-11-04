56a2fe6c1ba09e18ed23f29274940f11b3ec79c9
Andréa <andrea.marnier@gmail.com>
2025-11-04 06:32:11 +0100

    02 quest 1

--- 02_The_Song_of_Ducks_and_Dragons/quest_01.py
+++ 02_The_Song_of_Ducks_and_Dragons/quest_01.py
@@ -20,11 +20,26 @@ if __name__ == "__main__":
     args = _parse_args()
     t = time()
     with Path(f"inputs/{Path(__file__).stem}_{args.part}.txt").open("r") as file:
-        data = file.read().strip().split("\n")
+        data = file.read().strip().split("\n\n")
+    names = data[0].split(",")
+    instructions = data[1].split(",")
     if args.part == 1:
-        print(data)
+        idx = 0
+        for instruction in instructions:
+            mul = 1 if instruction[0] == "R" else -1
+            idx += int(instruction[1:]) * mul
+            idx = min(max(0, idx), len(names) - 1)
+        print(names[idx])
     elif args.part == 2:
-        raise NotImplementedError
+        idx = 0
+        for instruction in instructions:
+            mul = 1 if instruction[0] == "R" else -1
+            idx += int(instruction[1:]) * mul
+        print(names[idx % len(names)])
     else:
-        raise NotImplementedError
+        for instruction in instructions:
+            mul = 1 if instruction[0] == "R" else -1
+            idx = (int(instruction[1:]) * mul) % len(names)
+            names[0], names[idx] = names[idx], names[0]
+        print(names[0])
     print(time() - t)
--- 02_The_Song_of_Ducks_and_Dragons/stats.md
+++ 02_The_Song_of_Ducks_and_Dragons/stats.md
@@ -1,3 +1,7 @@
 |Quest|Part|Global Rank|Global Time|Local Time|
 |---|---|---|---|---|
-|
+|1|I|207|0|5h 34m 29s 469ms|6m 43s 994ms|
+|1|II|193|0|5h 36m 20s 675ms|8m 35s 200ms|
+|1|III|188|0|5h 40m 14s 050ms|12m 28s 575ms|
+
+Nice start with quest 1! Naive approaches are still enough at this point XD
