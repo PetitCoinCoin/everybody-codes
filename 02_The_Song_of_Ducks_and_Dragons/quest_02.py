26fc7517377e85e5cadde003bd4a3ca9278da020
Andréa <andrea.marnier@gmail.com>
2025-11-05 08:55:08 +0100

    02 quest 2

---
+++ 02_The_Song_of_Ducks_and_Dragons/quest_02.py
@@ -0,0 +1,60 @@
+import argparse
+import re
+
+from collections import namedtuple
+from pathlib import Path
+from time import time
+
+def _parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--part", "-p",
+        type=int,
+        choices={1, 2, 3},
+        help="Set puzzle part"
+    )
+    args = parser.parse_args()
+    if not args.part:
+        parser.error("Which part are you solving?")
+    return args
+
+Complex = namedtuple("Complex", "x y")
+
+def c_add(a: Complex, b: Complex) -> Complex:
+    return Complex(a.x + b.x, a.y + b.y)
+
+def c_mul(a: Complex, b: Complex) -> Complex:
+    return Complex(a.x * b.x - a.y * b.y, a.x * b.y + a.y * b.x)
+
+def c_div(a: Complex, b: Complex) -> Complex:
+    return Complex(int(a.x / b.x), int(a.y / b.y))
+
+def cycle(count: int, divider: Complex, base: Complex) -> Complex:
+    result = Complex(0, 0)
+    for _ in range(count):
+        result = c_mul(result, result)
+        result = c_div(result, divider)
+        result = c_add(result, base)
+        if not can_be_engraved(result):
+            return result
+    return result
+
+def can_be_engraved(a: Complex) -> bool:
+    bound = 1000000
+    return -bound <= a.x <= bound and -bound <= a.y <= bound
+
+if __name__ == "__main__":
+    args = _parse_args()
+    t = time()
+    with Path(f"inputs/{Path(__file__).stem}_{args.part}.txt").open("r") as file:
+        data = Complex(*tuple(int(x) for x in re.findall(r"A=\[(-?\d+),(-?\d+)\]", file.read())[0]))
+    if args.part == 1:
+        print(list(cycle(3, Complex(10, 10), data)))
+    else:
+        steps = 10 if args.part == 2 else 1
+        print(sum(
+            can_be_engraved(cycle(100, Complex(100000,100000), c_add(data, Complex(r, c))))
+            for r in range(0, 1001, steps)
+            for c in range(0, 1001, steps)
+        ))
+    print(time() - t)
--- 02_The_Song_of_Ducks_and_Dragons/stats.md
+++ 02_The_Song_of_Ducks_and_Dragons/stats.md
@@ -1,7 +1,14 @@
-|Quest|Part|Global Rank|Global Time|Local Time|
-|---|---|---|---|---|
+|Quest|Part|Global Rank|Global Score|Global Time|Local Time|
+|---|---|---|---|---|---|
 |1|I|207|0|5h 34m 29s 469ms|6m 43s 994ms|
 |1|II|193|0|5h 36m 20s 675ms|8m 35s 200ms|
 |1|III|188|0|5h 40m 14s 050ms|12m 28s 575ms|
+|2|I|59|0|11m 33s 106ms|11m 27s 945ms|
+|2|II|69|32|49m 32s 892ms|49m 27s 731ms|
+|2|III|72|79|55m 45s 836ms|55m 40s 675ms|
 
-Nice start with quest 1! Naive approaches are still enough at this point XD
+Nice start with quest 1! Naive approaches are still enough at this point :sweat_smile:
+
+So I tried this crazy idea for quest 2: go to bed, set an alarm 2min before midnight, and solve the quest with a fuzzy mind :laughing:. Not sure this will work for harder quests (python integer division already gave me a hard time here), but yeay 59/69/72 :tada:.
+Then back to bed, and clean up in the morning.
+It looks like `namedtuple` are really nice to ease the reading and comprehension, but they are less performant (part 3 runtime gets x2 with used of `namedtuple`). But on EC's Discord I discovered `Pypy` and wow. That's fast!
