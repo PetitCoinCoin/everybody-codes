476fe178720058f544af101a4501da191f27855a
Andréa <andrea.marnier@gmail.com>
2025-11-19 16:45:17 +0100

    utils: add grids prettyprint

--- utils/grids.py
+++ utils/grids.py
@@ -7,3 +7,10 @@ def get_next(position: tuple[int], *, with_diagonals: bool = False) -> Iterable[
     if with_diagonals:
         for dr, dc in ((1, 1), (1, -1), (-1, 1), (-1, -1)):
             yield (r + dr, c + dc)
+
+def pprint(grid: dict, max_r: int, max_c: int, blank: str = " ") -> None:
+    """Useful for debug or easter eggs"""
+    print("\n".join(
+        "".join(str(grid.get((r, c), blank)) for c in range(max_c + 1))
+        for r in range(max_r + 1)
+    ))
